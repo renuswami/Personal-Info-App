83c9bd7d84cbf93b1578908534abf5c821ce6c81
renuswami <renuswami2001@gmail.com>
2024-04-20 23:08:22 +0530

    About data class updated and in main activity optimized code !!

--- app/src/main/java/com/renu/personinformationdemo/About.kt
+++ app/src/main/java/com/renu/personinformationdemo/About.kt
@@ -2,6 +2,6 @@ package com.renu.personinformationdemo
 
 data class About(
     val name: String = "renu",
-    val age: String = "23",
-    val email: String = "1234512345"
+    val email: String = "renu03@gmail.com",
+    val age: String = "23"
 )
--- app/src/main/java/com/renu/personinformationdemo/MainActivity.kt
+++ app/src/main/java/com/renu/personinformationdemo/MainActivity.kt
@@ -9,14 +9,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.magnifier
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,8 +44,10 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize(),
                     ) {
                         Column(
-                            modifier = Modifier.align(Alignment.Center),
-                            verticalArrangement = Arrangement.Center
+                            modifier = Modifier.fillMaxSize()
+                                .padding(vertical = 8.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
                         ) {
                             Button(onClick = { mainDemoViewModel.setInfo() }) {
                                 Text(text = "Set Data")
@@ -51,9 +56,21 @@ class MainActivity : ComponentActivity() {
                                 Text(text = "Get Data")
                             }
                             val info = mainDemoViewModel.about.value
-                            Text(text = info.name, style = TextStyle(fontSize = 20.sp))
-                            Text(text = info.email, style = TextStyle(fontSize = 20.sp))
-                            Text(text = info.age, style = TextStyle(fontSize = 20.sp))
+                            TextField(
+                                value = TextFieldValue(info.name),
+                                onValueChange = {},
+                                label = { Text("Name: ") },
+                            )
+                            TextField(
+                                value = TextFieldValue(info.email),
+                                onValueChange = {},
+                                label = { Text("Email: ") },
+                            )
+                            TextField(
+                                value = TextFieldValue(info.age),
+                                onValueChange = {},
+                                label = { Text("Age: ") },
+                            )
                         }
                     }
                 }
